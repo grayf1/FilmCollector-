5f735dd606d85466d325de9f1d217b672984ea39
Faith Gray <grayf@chc.edu>
2022-02-03 10:42:01 -0500

    Started Create Actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,22 @@
 class ActorsController < ApplicationController 
+	# index method gets called when the actors URL is requested. 
+	# index method gets mapped to actors index.html.erb.
 	def index 
+		# call all method on Actor model class. 
+		# all method selects all of data in the actor table 
+		# and returns the data as an array of objects. 
+		# store the array of objects in an instance variable. 
+		# instance variable is available to actors index.html.erb
 		@actors = Actor.all
+	end
+
+	# new method gets called when the actors/new URL is requested.
+	# new method is mapped to the actors new.html.erb 
+	def new	
+	end 
+
+	# create method gets called when the Create button is pushed on 
+	# the actors new.html.erb.
+	def create
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# each route consists of 3 parts: 1. HTTP method, 2. URL to web page 
+# view being requested, 3. name of Ruby class and method envoked when 
+# URL to web page view is requested.
 Rails.application.routes.draw do
+  # when a get request is made for the actors URL, 
+  # Rails envokes the index method in the actors Ruby controller class. 
+  # Rails automatically maps the index method to a web page view named 
+  #index.html.erb and serves the web page to the browser where its rendered.
   get "actors" => "actors#index"
+  get "actors/new" => "actors#new"
+  # when a post request is made for the actors to the URL, 
+  # Rails envokes the create method in the actors Ruby Controller class
+  post "actors" => "actors#create"
 end
